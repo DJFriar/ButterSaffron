28e27d58e4598fe5d4b13159cb37157367565edd
Tommy Craft <38347315+DJFriar@users.noreply.github.com>
2024-01-21 21:34:52 -0600

    Pricing update

--- private/routes.js
+++ private/routes.js
@@ -75,11 +75,11 @@ module.exports = function (app) {
   app.get("/tables", async (req,res) => {
     var menuItems = [
       { name: 'Dessert Buffet', image: 'DessertBuffet.jpg', description: '', allergens: '' },
-      { name: 'Mini Cream Puffs', image: 'MiniCreamPuffs.jpg', description: '$30/dozen', allergens: '' },
-      { name: 'Mini Fruit Tarts', image: 'MiniFruitTarts2.jpg', description: '$30/dozen', allergens: '' },
-      { name: 'Macarons', image: 'NoImage.png', description: '$30/dozen', allergens: '' },
-      { name: 'Shortbread Cookies', image: 'NoImage.png', description: '$30/dozen', allergens: '' },
-      { name: 'Logo Cookies', image: 'LogoCookies.jpg', description: '$30/dozen, can be customized with a logo or image of your choice.', allergens: '' },
+      { name: 'Mini Cream Puffs', image: 'MiniCreamPuffs.jpg', description: '$36/dozen', allergens: '' },
+      { name: 'Mini Fruit Tarts', image: 'MiniFruitTarts2.jpg', description: '$36/dozen', allergens: '' },
+      { name: 'Macarons', image: 'NoImage.png', description: '$36/dozen', allergens: '' },
+      { name: 'Shortbread Cookies', image: 'NoImage.png', description: '$36/dozen', allergens: '' },
+      { name: 'Logo Cookies', image: 'LogoCookies.jpg', description: '$36/dozen, can be customized with a logo or image of your choice.', allergens: '' },
       { name: 'Setup & Stand Rental', image: 'DessertSetup.jpg', description: 'Starting at $25, plus a refundable deposit.', allergens: '' }
     ]
     res.render("pages/tables", {menuItems: menuItems });
